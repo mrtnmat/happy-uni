709308bcef2389c502092794f0c3e47329d52346
Matteo <mrtn.matteo@protonmail.com>
2022-11-09 13:07:20 +0100

    addFaculty reads data from TextView and catches exceptions

--- app/src/main/java/com/example/happyuni/activities/AddFacultyActivity.kt
+++ app/src/main/java/com/example/happyuni/activities/AddFacultyActivity.kt
@@ -2,13 +2,14 @@ package com.example.happyuni.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 import com.example.happyuni.R
 
 import kotlinx.coroutines.*
 
-import android.util.Log
+import android.widget.EditText
 import androidx.room.Room
 import com.example.happyuni.AppDatabase
 import com.example.happyuni.entities.Faculty
@@ -18,7 +19,9 @@ class AddFacultyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_faculty)
 
-
+        val facultyCode = findViewById<EditText>(R.id.facultyCode)
+        val facultyName = findViewById<EditText>(R.id.facultyName)
+        val facultyLocation = findViewById<EditText>(R.id.facultyLocation)
 
         val db = Room.databaseBuilder(
             applicationContext,
@@ -30,7 +33,15 @@ class AddFacultyActivity : AppCompatActivity() {
             runBlocking {
                 launch (Dispatchers.Default) {
                     val facultyDao = db.facultyDao()
-                    facultyDao.insertFaculties(Faculty(1, "Scienze", "Piazza Grande"))
+                    try {
+                        facultyDao.insertFaculties(Faculty(
+                            Integer.parseInt(facultyCode.text.toString()),
+                            facultyName.text.toString(),
+                            facultyLocation.text.toString()
+                        ))
+                    } catch (e : Exception) {
+                        Log.e("add faculty", e.toString())
+                    }
                 }
             }
         }
