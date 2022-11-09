2e4ab3133d7cbaa9d7b119f361876ef02b7981fa
Matteo <mrtn.matteo@protonmail.com>
2022-11-09 11:01:56 +0100

    first commmit

---
+++ app/src/main/java/com/example/happyuni/AddFacultyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.happyuni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFacultyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_faculty)
+    }
+}
---
+++ app/src/main/java/com/example/happyuni/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.happyuni
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnAddFaculty : Button = findViewById<Button>(R.id.btnAddFaculty)
+        btnAddFaculty.setOnClickListener {
+            startActivity(Intent(this, AddFacultyActivity::class.java))
+        }
+
+        Log.d("onCreate", "hello!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("onStart", "hello!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("onResume", "hello!")
+    }
+}
