e8a228155c5ae961cfd324c79738c9c678d57a95
Matteo <mrtn.matteo@protonmail.com>
2022-11-09 12:11:44 +0100

    basic insert button functionality with co-rountines

--- app/src/main/java/com/example/happyuni/activities/AddFacultyActivity.kt
+++ app/src/main/java/com/example/happyuni/activities/AddFacultyActivity.kt
@@ -2,11 +2,38 @@ package com.example.happyuni.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 import com.example.happyuni.R
+
+import kotlinx.coroutines.*
+
+import android.util.Log
+import androidx.room.Room
+import com.example.happyuni.AppDatabase
+import com.example.happyuni.entities.Faculty
 
 class AddFacultyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_faculty)
+
+        val facultyAdd : Button = findViewById(R.id.facultyAdd)
+        facultyAdd.setOnClickListener {
+            runBlocking {
+                launch (Dispatchers.Default) {
+                    val db = Room.databaseBuilder(
+                        applicationContext,
+                        AppDatabase::class.java, "appDb"
+                    ).build()
+                    val facultyDao = db.facultyDao()
+                    facultyDao.insertFaculties(Faculty(1, "Scienze", "Piazza Grande"))
+                }
+            }
+        }
+    }
+
+    fun test() = runBlocking {
+
     }
 }
